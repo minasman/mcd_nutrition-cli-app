ba180525a57d34bfb4975f5d9cf34fa543156059
Daniel Hernandez <elreymcd@yahoo.com>
2018-06-12 14:58:53 -0500

    added options for meals, HM and suace

--- lib/test.rb
+++ lib/test.rb
@@ -13,14 +13,25 @@ class Welcome
     end
     input = gets.strip until input.to_i > 0 && input.to_i <= menu.size
     puts "You Selected #{menu[input.to_i - 1].flatten[0]}"
+    category = menu[input.to_i - 1].flatten[0]
+    case category
+    when "Extra Value Meal"
+      select_meal(Scraper.new.scrape_category_for_list("https://www.mcdonalds.com#{menu[input.to_i - 1].flatten[1]}"))
+    when "Happy Meal"
+      select_happy_meal(Scraper.new.scrape_category_for_list("https://www.mcdonalds.com#{menu[input.to_i - 1].flatten[1]}"))
+    when "Sauces"
+      select_sauces(Scraper.new.scrape_category_for_list("https://www.mcdonalds.com#{menu[input.to_i - 1].flatten[1]}"))
+    else
     select_item(Scraper.new.scrape_category_for_list("https://www.mcdonalds.com#{menu[input.to_i - 1].flatten[1]}"), menu[input.to_i - 1].flatten[0])
+    end
   end
 
   def select_item(category, input)
     puts "Please select an item from #{input}"
     category.each_with_index {|item, i| puts "#{i + 1}: #{item.flatten[0]}"}
     input = gets.strip until input.to_i > 0 && input.to_i <= category.size
-    puts "You selected #{category[input.to_i - 1].flatten[0]}"
+    item = category[input.to_i - 1].flatten[0]
+    puts "You selected #{item}"
     get_nutrition(Scraper.new.scrape_nutrition_info("https://www.mcdonalds.com#{category[input.to_i - 1].flatten[1]}"), category[input.to_i - 1].flatten[0])
   end
 
