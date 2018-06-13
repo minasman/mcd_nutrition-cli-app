1bf9eb0a9881d48534c3b3157fb6c9c0326d9da1
Daniel Hernandez <elreymcd@yahoo.com>
2018-06-12 23:06:42 -0500

    still working on getting this right

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,59 +19,33 @@ class Scraper
     category_list = []
     url = "https://fastfoodnutrition.org#{category.flatten[1]}"
     site = Nokogiri::HTML(open(url))
-    site = site.css("div.container.main_container div #contentarea a.toggle_category.toggle_div")
+    site = site.css("#contentarea a.toggle_category.toggle_div")
     site.each do |item|
-      category_list << item.css("h2").text
+      category_list << item.css("h2").text unless item.css("h2").text == "Most Popular Items"
     end
     category_list
   end
 
-  def scrape_evm_list(category)
-    item_list = []
-    site = Nokogiri::HTML(open(category))
-    site = site.css(".zoom-anim-parent")
-    site.each do |item|
-      item_list << item.css("div div div div h3").text
-    end
-    item_list
-  end
-
-  def scrape_evm_nutrition_info(item_picked, meal_site)
-    nutrition_site = []
-    mock = Watir::Browser.new :chrome, headless: true
-    mock_site = mock.goto meal_site
-
-    binding.pry
-    site = Nokogiri::HTML.parse(mock.html)
-    site = site.css(".zoom-anim-parent")
-    site.each do |item|
-        puts "Item = #{item_picked} and I am at #{item.css("div div div div h3").text}"
-      if item.css("div div div div h3").text == item_picked
-        puts "found #{item_picked}"
-        puts item.css("div div div div p a").attribute("href").value
-        mock_site.links(:text =>'Learn more about Egg McMuffin').click
-        binding.pry
+  def scrape_category_items(item_name, index, item_site)
+      item_list = []
+      site = Nokogiri::HTML(open(item_site))
+      site = site.css("#contentarea div.categories")[index].css("ul div li")
+      site.each do |item|
+        item_list << {item.css("a").text => item.css("a").attribute("href").value}
       end
-    end
+      item_list
   end
 
-  def scrape_nutrition_info(item)
-    i = 0
+  def scrape_nutrition_info(item) #get a hash
+    index = 2
     nutrition_list = []
-    mock = Watir::Browser.new :chrome, headless: true
-    mock_site = mock.goto item
-    site = Nokogiri::HTML.parse(mock.html)
-    site = site.css(".nutrition-numbers")
-    while i < 4 do
-      nutrition_list << {site.css("div div ul li")[i].css("div.type span")[0].text => site.css("div div ul li")[i].css("div.number span")[0].text}
-      i += 1
+    url = "https://fastfoodnutrition.org#{item.flatten[1]}"
+    site = Nokogiri::HTML(open(url))
+    site = site.css(".item_nutrition tbody tr")
+    binding.pry
+    site.each do |item|
+
     end
-    i = 0
-    while i < 11 do
-      nutrition_list << {site.css("div.detailed div ul li")[i].css("span")[0].text => site.css("div.detailed div ul li")[i].css("span")[1].text}
-      i += 1
-    end
-    nutrition_list
   end
 
   def test
--- lib/test.rb
+++ lib/test.rb
@@ -11,23 +11,33 @@ class Welcome
     restaurants.each_with_index {|restaurant, i| puts "#{i + 1}: #{restaurant.flatten[0]}"}
     selection = gets.strip.to_i
     puts "\nYou selected #{restaurants[selection - 1].flatten[0]}"
-    select_category(Scraper.new.scrape_restaurant_categories(restaurants[selection - 1]))
+    select_category(Scraper.new.scrape_restaurant_categories(restaurants[selection - 1]),"https://fastfoodnutrition.org#{restaurants[selection - 1].flatten[1]}")
   end
 
-  def select_category(categories)
-    puts "\nPlease select a Menu Item:"
+  def select_category(categories, item_site)
+    puts "\nPlease select a Category of Menu Items:"
     categories.each_with_index {|category, i| puts "#{i + 1}: #{category}"}
     selection = gets.strip.to_i
     puts "\nYou selected #{categories[selection - 1]}"
-    select_item(Scraper.new.scrape_category_items(categories[selection - 1], selection - 1))
+    select_item(Scraper.new.scrape_category_items(categories[selection - 1], selection, item_site))
   end
 
-  def select_item(category, input)
-    puts "Please select an item from #{input}:"
+  def select_item(menu_item_list)
+    i = 1
+    puts "Please select an item:"
+    menu_item_list.each do |item|
+      puts "#{i}: #{item.flatten[0]}"
+      i += 1
+    end
+    selection = gets.strip.to_i
+    puts "You selected #{menu_item_list[selection - 1].flatten[0]}"
+    binding.pry
+    get_nutrition(Scraper.new.scrape_nutrition_info(menu_item_list[selection - 1]))
   end
 
   def get_nutrition(nutrition_list, item_name)
     puts "\nHere is the nutrition information for #{item_name}:"
+    
     nutrition_list.each do |item|
       puts "#{item.flatten[0]}: #{item.flatten[1]}"
     end
